86201c1c15db3988d4f8d27091792b7b839b3ed3
jasper <jasper.desnyder@student.howest.be>
2022-04-22 21:16:39 +0200

    Added scroll to top button

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,34 @@
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    console.log("test");
+    const $links = document.querySelectorAll(".menu-links a");
+    $links.forEach(link => {
+        link.addEventListener("click", closeNav);
+    });
+
+    window.onscroll = function () { displayButton() };
+
+    const $scrollToTopButton = document.querySelector("#go-to-top button");
+    $scrollToTopButton.addEventListener("click", goToTop)
+}
+
+function closeNav(e) {
+    const $checkBox = document.querySelector("#openmenu");
+    $checkBox.checked = false;
+}
+
+function displayButton() {
+    const $scrollToTopButton = document.querySelector("#go-to-top button");
+
+    if (document.documentElement.scrollTop > 20) {
+        $scrollToTopButton.style.display = "block";
+    } else {
+        $scrollToTopButton.style.display = "none";
+    }
+}
+
+function goToTop(e) {
+    e.preventDefault();
+
+    document.documentElement.scrollTop = 0;
 }
